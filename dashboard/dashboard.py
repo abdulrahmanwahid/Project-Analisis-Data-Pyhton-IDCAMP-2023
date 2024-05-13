e8df6649e8781ec1079223ec874bfdb96ac9b888
abdulrahmanwahid <104683904+abdulrahmanwahid@users.noreply.github.com>
2024-05-13 16:40:15 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,7 +11,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv(""https://raw.githubusercontent.com/mhdhfzz/data-analyst-dicoding/main/dashboard/df.csv"")
+all_df = pd.read_csv("https://raw.githubusercontent.com/mhdhfzz/data-analyst-dicoding/main/dashboard/df.csv"")
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
